b3353eaf13b882d90052a93ef89a16edc45dc4a7
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2023-10-13 23:36:13 +0100

    added autofollow

---
+++ PandorasBox/Features/Actions/AutoFollow.cs
@@ -0,0 +1,110 @@
+using ECommons.DalamudServices;
+using Automaton.FeaturesSetup;
+using System;
+using Automaton.Helpers;
+using System.Linq;
+using Dalamud.Game.Command;
+using System.Collections.Generic;
+using ECommons;
+using Dalamud.Game.ClientState.Objects.Types;
+using System.Numerics;
+using ImGuiNET;
+
+namespace Automaton.Features.Other
+{
+    public unsafe class AutoFollow : Feature
+    {
+        public override string Name => "Auto Follow";
+
+        public override string Description => "True Auto Follow. Trigger with /autofollow while targeting someone. Use it with no target to wipe the current master.";
+
+        public override FeatureType FeatureType => FeatureType.Actions;
+
+        public Configs Config { get; private set; }
+        public class Configs : FeatureConfig
+        {
+            [FeatureConfigOption("Distance to Keep", "", 2, IntMin = 0, IntMax = 30, EditorSize = 300)]
+            public int distanceToKeep = 3;
+        }
+
+        protected override DrawConfigDelegate DrawConfigTree => (ref bool hasChanged) =>
+        {
+            ImGui.PushItemWidth(300);
+            if (ImGui.SliderInt("Distance to Keep (yalms)", ref Config.distanceToKeep, 0, 30)) hasChanged = true;
+
+            ImGui.Text($"Current Master: {(master != null ? master.Name : "null")}");
+            ImGui.Text($"Your Position: x: {Svc.ClientState.LocalPlayer.Position.X}, y: {Svc.ClientState.LocalPlayer.Position.Y}, z: {Svc.ClientState.LocalPlayer.Position.Z}");
+            ImGui.Text($"Master Position: x: {(master != null ? master.Position.X : "null")}, y: {(master != null ? master.Position.Y : "null")}, z: {(master != null ? master.Position.Z : "null")}");
+            ImGui.Text($"Distance to Master: {(master != null ? Vector3.Distance(Svc.ClientState.LocalPlayer.Position, master.Position) : "null")}");
+
+            if (ImGui.Button("Set")) { master = Svc.Targets.Target; }
+            ImGui.SameLine();
+            if (ImGui.Button("Clear")) { master = null; }
+        };
+
+        public string Command { get; set; } = "/autofollow";
+        private readonly List<string> registeredCommands = new();
+
+        private GameObject master;
+        private readonly OverrideMovement movement = new();
+
+        protected void OnCommand(List<string> args)
+        {
+            try
+            {
+                master = Svc.Targets.Target;
+            }
+            catch (Exception e) { e.Log(); }
+        }
+
+        protected virtual void OnCommandInternal(string _, string args)
+        {
+            args = args.ToLower();
+            OnCommand(args.Split(' ').ToList());
+        }
+
+        public override void Enable()
+        {
+            Config = LoadConfig<Configs>() ?? new Configs();
+            if (Svc.Commands.Commands.ContainsKey(Command))
+            {
+                Svc.Log.Error($"Command '{Command}' is already registered.");
+            }
+            else
+            {
+                Svc.Commands.AddHandler(Command, new CommandInfo(OnCommandInternal)
+                {
+                    HelpMessage = "",
+                    ShowInHelp = false
+                });
+
+                registeredCommands.Add(Command);
+            }
+
+            Svc.Framework.Update += Follow;
+            base.Enable();
+        }
+
+        public override void Disable()
+        {
+            SaveConfig(Config);
+            foreach (var c in registeredCommands)
+            {
+                Svc.Commands.RemoveHandler(c);
+            }
+            registeredCommands.Clear();
+
+            Svc.Framework.Update -= Follow;
+            base.Disable();
+        }
+
+        private void Follow(IFramework framework)
+        {
+            if (master == null) { movement.Enabled = false; return; }
+            if (Vector3.Distance(Svc.ClientState.LocalPlayer.Position, master.Position) <= Config.distanceToKeep) { movement.Enabled = false; return; }
+
+            movement.Enabled = true;
+            movement.DesiredPosition = master.Position;
+        }
+    }
+}
--- PandorasBox/Helpers/NumberHelper.cs
+++ PandorasBox/Helpers/NumberHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Numerics;
 using System.Text;
 
 namespace Automaton.Helpers
@@ -31,5 +32,60 @@ namespace Automaton.Helpers
 
             return "now";
         }
+
+        public struct Angle(float radians = 0)
+        {
+            public const float RadToDeg = 180 / MathF.PI;
+            public const float DegToRad = MathF.PI / 180;
+
+            public float Rad = radians;
+            public float Deg => Rad * RadToDeg;
+
+            public static Angle FromDirection(Vector2 dir) => FromDirection(dir.X, dir.Y);
+            public static Angle FromDirection(float x, float z) => new(MathF.Atan2(x, z));
+            public readonly Vector2 ToDirection() => new(Sin(), Cos());
+
+            public static Angle operator +(Angle a, Angle b) => new(a.Rad + b.Rad);
+            public static Angle operator -(Angle a, Angle b) => new(a.Rad - b.Rad);
+            public static Angle operator -(Angle a) => new(-a.Rad);
+            public static Angle operator *(Angle a, float b) => new(a.Rad * b);
+            public static Angle operator *(float a, Angle b) => new(a * b.Rad);
+            public static Angle operator /(Angle a, float b) => new(a.Rad / b);
+            public readonly Angle Abs() => new(Math.Abs(Rad));
+            public readonly float Sin() => MathF.Sin(Rad);
+            public readonly float Cos() => MathF.Cos(Rad);
+            public readonly float Tan() => MathF.Tan(Rad);
+            public static Angle Asin(float x) => new(MathF.Asin(x));
+            public static Angle Acos(float x) => new(MathF.Acos(x));
+
+            public readonly Angle Normalized()
+            {
+                var r = Rad;
+                while (r < -MathF.PI)
+                    r += 2 * MathF.PI;
+                while (r > MathF.PI)
+                    r -= 2 * MathF.PI;
+                return new(r);
+            }
+
+            public bool AlmostEqual(Angle other, float epsRad)
+            {
+                var delta = Math.Abs(Rad - other.Rad);
+                return delta <= epsRad || delta >= (2 * MathF.PI) - epsRad;
+            }
+
+            public static bool operator ==(Angle l, Angle r) => l.Rad == r.Rad;
+            public static bool operator !=(Angle l, Angle r) => l.Rad != r.Rad;
+            public override readonly bool Equals(object? obj) => obj is Angle angle && this == angle;
+            public override readonly int GetHashCode() => Rad.GetHashCode();
+            public override string ToString() => Deg.ToString("f0");
+        }
+
+    }
+    public static class AngleExtensions
+    {
+        public static NumberHelper.Angle Radians(this float radians) => new(radians);
+        public static NumberHelper.Angle Degrees(this float degrees) => new(degrees * NumberHelper.Angle.DegToRad);
+        public static NumberHelper.Angle Degrees(this int degrees) => new(degrees * NumberHelper.Angle.DegToRad);
     }
 }
